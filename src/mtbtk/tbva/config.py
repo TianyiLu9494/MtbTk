54f7d06bc6a8bc8eb8bc58ff3937e02e4d712207
TianyiLu (陆天翊） <tianyi9494@gmail.com>
2024-09-29 16:42:57 +0800

    add Epitopes Domains loading

--- src/mtbtk/tbva/config.py
+++ src/mtbtk/tbva/config.py
@@ -5,4 +5,22 @@ def load_genes():
     with genes_file.open('r') as f:
         return json.load(f)
 
+def load_lineage_info():
+    lineage_info_file = DATA_DIR / 'lineage_info.json'
+    with lineage_info_file.open('r') as f:
+        return json.load(f)
+
+def load_domains():
+    domains_file = DATA_DIR / 'domains.json'
+    with domains_file.open('r') as f:
+        return json.load(f)
+
+def load_epitopes():
+    epitopes_file = DATA_DIR / 'epitopes.json'
+    with epitopes_file.open('r') as f:
+        return json.load(f)
+
 genes = load_genes()
+lineage_info = load_lineage_info()
+domains = load_domains()
+epitopes = load_epitopes()
