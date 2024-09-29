690ea6bb340a6523aa30b016973cfae888d8c2ca
TianyiLu (陆天翊） <tianyi9494@gmail.com>
2024-09-29 23:56:56 +0800

    Create __init__.py

---
+++ src/mtbtk/tbgea/__init__.py
@@ -0,0 +1,4 @@
+from .enrichment_analyzer import BaseEnrichmentAnalyzer, GOEnrichmentAnalyzer, KEGGEnrichmentAnalyzer, TFEnrichmentAnalyzer
+from .config import GOs, KEGG_pathways, TF_genes
+__all__ = ["BaseEnrichmentAnalyzer", "GOEnrichmentAnalyzer", "KEGGEnrichmentAnalyzer", "TFEnrichmentAnalyzer", "GOs", "KEGG_pathways", "TF_genes"]
+
