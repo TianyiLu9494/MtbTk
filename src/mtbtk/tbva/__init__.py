3ecf0af1b672073c2aabbf777d82be59e8e0ec4e
TianyiLu (陆天翊） <tianyi9494@gmail.com>
2024-11-07 21:17:37 +0800

    add SnpEffectAnnotator exportation

--- src/mtbtk/tbva/__init__.py
+++ src/mtbtk/tbva/__init__.py
@@ -1,6 +1,7 @@
 from .vcf_parser import VCFParser  # 假设你的 VCFParser 类在 vcf_parser.py 文件中
 from .lineage_identifier import LineageIdentifier
 from .annotator import VariantAnnotator
+from .SNP_effect_annotator import SnpEffectAnnotator
 from .config import genes
 
-__all__ = ['genes','VCFParser','VariantAnnotator','LineageIdentifier'] 
+__all__ = ['genes','VCFParser','VariantAnnotator','LineageIdentifier','SnpEffectAnnotator'] 
